8e10ecf95628a547b76933177fc8b4ff0cfe6d05
Jacob Espinoza <jtespi@nmsu.edu>
2018-10-02 21:26:39 -0700

    Added fragment for displaying one question

---
+++ app/src/main/java/com/techexchange/mobileapps/assignment2/OneQuestionFrag.java
@@ -0,0 +1,124 @@
+package com.techexchange.mobileapps.assignment2;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link OneQuestionFrag.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link OneQuestionFrag#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OneQuestionFrag extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public OneQuestionFrag() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment OneQuestionFrag.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static OneQuestionFrag newInstance(String param1, String param2) {
+        OneQuestionFrag fragment = new OneQuestionFrag();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_question_list, container, false);
+
+        Button submitButton = view.findViewById(R.id.btn_submit);
+        TextView questionTxt = view.findViewById(R.id.oneQuestionTxt);
+        RadioButton rb_one = view.findViewById(R.id.radio1);
+        RadioButton rb_two = view.findViewById(R.id.radio2);
+        RadioButton rb_three = view.findViewById(R.id.radio3);
+        RadioButton rb_four = view.findViewById(R.id.radio4);
+
+
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
